2c2114b464c953e7e5c09cdb861dd2d46f3d171f
moonshadow <last_enemy@naver.com>
2020-11-23 22:23:43 +0900

    201123

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = "700";
+const saveBtn = document.getElementById("jsSave");
 
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = "2.5";
@@ -63,12 +66,25 @@ function handleCanvasClick() {
   }
 }
 
+function handleCM(event) {
+  event.preventDefault;
+}
+
+function handleSaveClick() {
+  const image = canvas.toDataURL("image");
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "PaintJS🎨";
+  link.click();
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
   canvas.addEventListener("click", handleCanvasClick);
+  canvas.addEventListener("contextmenu", handleCM);
 }
 
 Array.from(colors).forEach((color) =>
@@ -81,4 +97,8 @@ if (range) {
 
 if (mode) {
   mode.addEventListener("click", handleModeClick);
+}
+
+if (saveBtn) {
+  saveBtn.addEventListener("click", handleSaveClick);
 }
